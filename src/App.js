cb1e2de867126c73825684ba88d195d164cf2e19
AmoDare <amoldare21@gmail.com>
2019-05-11 23:41:41 +0100

    new list functions

--- src/App.js
+++ src/App.js
@@ -37,6 +37,12 @@ class App extends Component{
     this.setState({showPersons:!doesShow});
 
    }
+
+   deletePersonHandler=(personIndex)=>{
+     const persons=this.state.persons;
+     persons.splice(personIndex,1);
+     this.setState({persons:persons})
+   }
   render(){
     const style={
       backgroundColour: 'white',
@@ -50,17 +56,13 @@ class App extends Component{
     if(this.state.showPersons){
       persons=(
         <div>
-      <Person name={this.state.persons[0].name} 
-      age={this.state.persons[0].age}
-      click={()=>this.switchNameHandler('gyt')}
-      changed={this.nameChangeHandler}
-      >
-      
-      </Person>
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+      {
+        this.state.persons.map((person ,index)=>{
+          return <Person click={()=>this.deletePersonHandler(index)} name={person.name} age={person.age}/>
+        })
+      }
       </div> 
-      )
+      );
     }
     return (
       <div className="App">
